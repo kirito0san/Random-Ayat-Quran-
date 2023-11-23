861fa92dba8e69db4ae4b3613591704fa3fa6477
kirito0san <jodat.ahmedsyed@gmail.com>
2023-11-23 18:22:47 +0200

    a

--- src/Ts.ts
+++ src/Ts.ts
@@ -3,19 +3,20 @@ let Aya = document.querySelector(`.Aya`) as HTMLParagraphElement;
 let click = document.querySelector(`.click`) as HTMLImageElement;
 let names = document.querySelector(`.name`) as HTMLParagraphElement;
 let sound = document.querySelector(`.sound`) as HTMLAudioElement;
-// click.addEventListener(`click`, () => {
-//   fetch("/data.json")
-//     .then((e) => e.json())
-//     .then((e) => {
-//       let one = Math.ceil(Math.random() * 113);
-//       let len = e.data.surahs[one].ayahs.length;
-//       let qran = e.data.surahs[one].ayahs[Math.floor(Math.random() * len)];
-//       sound.src = qran.audio;
-//       Aya.innerHTML = qran.text;
-//       MyId.innerHTML = len;
-//       names.innerHTML = e.data.surahs[one].name;
-//     });
-// });
+click.addEventListener(`click`, () => {
+  fetch("/data.json")
+    .then((e) => e.json())
+    .then((e) => {
+      console.log(e);
+      let one = Math.ceil(Math.random() * 113);
+      let len = e.data.surahs[one].ayahs.length;
+      let qran = e.data.surahs[one].ayahs[Math.floor(Math.random() * len)];
+      sound.src = qran.audio;
+      Aya.innerHTML = qran.text;
+      MyId.innerHTML = len;
+      names.innerHTML = e.data.surahs[one].name;
+    });
+});
 fetch("/data.json")
   .then((e) => e.json())
   .then((e) => {
