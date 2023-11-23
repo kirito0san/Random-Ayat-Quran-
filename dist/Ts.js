32c2c532b3ba8787077645d66c94264e10c7bbd8
kirito0san <jodat.ahmedsyed@gmail.com>
2023-11-23 06:37:11 +0200

    done

---
+++ dist/Ts.js
@@ -0,0 +1,30 @@
+"use strict";
+let MyId = document.querySelector(`.id`);
+let Aya = document.querySelector(`.Aya`);
+let click = document.querySelector(`.click`);
+let names = document.querySelector(`.name`);
+let sound = document.querySelector(`.sound`);
+click.addEventListener(`click`, () => {
+    fetch("http://api.alquran.cloud/v1/quran/ar.alafasy")
+        .then((e) => e.json())
+        .then((e) => {
+        let one = Math.ceil(Math.random() * 113);
+        let len = e.data.surahs[one].ayahs.length;
+        let qran = e.data.surahs[one].ayahs[Math.floor(Math.random() * len)];
+        sound.src = qran.audio;
+        Aya.innerHTML = qran.text;
+        MyId.innerHTML = len;
+        names.innerHTML = e.data.surahs[one].name;
+    });
+});
+fetch("http://api.alquran.cloud/v1/quran/ar.alafasy")
+    .then((e) => e.json())
+    .then((e) => {
+    let one = Math.ceil(Math.random() * 113);
+    let len = e.data.surahs[one].ayahs.length;
+    let qran = e.data.surahs[one].ayahs[Math.floor(Math.random() * len)];
+    sound.src = qran.audio;
+    Aya.innerHTML = qran.text;
+    MyId.innerHTML = len;
+    names.innerHTML = e.data.surahs[one].name;
+});
---
+++ src/Ts.ts
@@ -0,0 +1,29 @@
+let MyId = document.querySelector(`.id`) as HTMLSpanElement;
+let Aya = document.querySelector(`.Aya`) as HTMLParagraphElement;
+let click = document.querySelector(`.click`) as HTMLImageElement;
+let names = document.querySelector(`.name`) as HTMLParagraphElement;
+let sound = document.querySelector(`.sound`) as HTMLAudioElement;
+click.addEventListener(`click`, () => {
+  fetch("http://api.alquran.cloud/v1/quran/ar.alafasy")
+    .then((e) => e.json())
+    .then((e) => {
+      let one = Math.ceil(Math.random() * 113);
+      let len = e.data.surahs[one].ayahs.length;
+      let qran = e.data.surahs[one].ayahs[Math.floor(Math.random() * len)];
+      sound.src = qran.audio;
+      Aya.innerHTML = qran.text;
+      MyId.innerHTML = len;
+      names.innerHTML = e.data.surahs[one].name;
+    });
+});
+fetch("http://api.alquran.cloud/v1/quran/ar.alafasy")
+  .then((e) => e.json())
+  .then((e) => {
+    let one = Math.ceil(Math.random() * 113);
+    let len = e.data.surahs[one].ayahs.length;
+    let qran = e.data.surahs[one].ayahs[Math.floor(Math.random() * len)];
+    sound.src = qran.audio;
+    Aya.innerHTML = qran.text;
+    MyId.innerHTML = len;
+    names.innerHTML = e.data.surahs[one].name;
+  });
